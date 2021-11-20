eb50af014328c218ed003e18c0d0602ae8f16965
Zi-on <zioniflores12@yahoo.com>
2021-11-20 16:02:51 -0600

    added css styiling to my calculator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="calculator-grid">
       <div className="output">
@@ -9,6 +10,7 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
+      <button>÷</button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
